a06f506a65f277510d18742fe537591f6393e1d0
Guilherme Brandão de Souza <guilherme.souza@alunos.bpkedu.com.br>
2024-10-29 21:18:15 -0300

    feat: cadastro produtos

--- js/produto.js
+++ js/produto.js
@@ -1,5 +1,5 @@
 async function fetchProdutos() {
-    const queryURL = "https://829cdhmq.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27produtos%27%5D+%7B%0A++nome%2C%0A++preco%2C%0A++nota%2C%0A++%22foto%22%3A+foto.asset-%3Eurl%0A%7D";
+    const queryURL = "https://829cdhmq.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27produtos%27%5D+%7B%0A++nome%2C%0A++preco%2C%0A++nota%2C%0A++%22foto%22%3A+foto.asset-%3Eurl%0A%7D"
 
     try {
         const response = await fetch(queryURL);
@@ -13,36 +13,36 @@ async function fetchProdutos() {
         renderProdutos(data.result);
 
     } catch (error) {
-        console.error("Erro ao buscar as avaliações:", error);
+        console.error("Erro ao buscar os produtos:", error);
     }
 }
 
+
+
 function renderProdutos(produtos) {
-    const container = document.getElementById('testimonial-container');
+    const container = document.getElementById('product');
     container.innerHTML = '';
 
     produtos.forEach(produtos => {
         const produtoHTML = `
-        <div class="small-container">
-        
-        <div class="row">
             <div class="col-4">
                 <a href="produto.html">
-                    <img src="./images/velas/desjardins_4.jpg">
+                    <img src="${produtos.foto || './images/default-user.png'}" alt="Foto de ${produtos.nome || 'Usuário'}">
                 </a>
                 <h4>${produtos.nome || 'Produto não disponível'}</h4>
                 <div class="rating">
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>z
+                    <i class="fa fa-star"></i>
                     <i class="fa fa-star-o"></i>
                 </div>
-                <p>${produtos.nome || 'Depoimento não disponível'}</p>
+                <p>${produtos.preco || 'Produto não disponível'}</p>
             </div>   
-        </div>
-    //     container.innerHTML += depoimentoHTML;
-    // });
+    `;
+
+         container.innerHTML += produtoHTML;
+     });
 }
 
 fetchProdutos();
